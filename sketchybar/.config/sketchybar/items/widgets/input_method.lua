4e51d8d812802e78a095843741ba4a9110360faa
herofuzq <herofuzq@gmail.com>
2026-05-17 23:08:33 +0800

    change

--- sketchybar/.config/sketchybar/items/widgets/input_method.lua
+++ sketchybar/.config/sketchybar/items/widgets/input_method.lua
@@ -11,35 +11,35 @@ local im_map = {
 }
 
 local input_method = sbar.add("item", "widgets.input_method", {
-	position = "right",
-	padding_left = 2,
-	padding_right = 2,
-	icon = {
-		font = {
-			family = fonts.font_fira.text,
-			style = fonts.font_fira.style_map["Bold"],
-			size = fonts.font_fira.size,
-		},
-		padding_left = settings.padding.icon_label_item.icon.padding_left,
-		padding_right = 2,
-		color = colors.active.deep_blue,
-	},
-	label = {
-		font = {
-			family = fonts.font_fira.text,
-			style = fonts.font_fira.style_map["Bold"],
-			size = fonts.font_fira.size,
-		},
-		padding_left = 0,
-		padding_right = settings.padding.icon_label_item.label.padding_right,
-		color = colors.active.sep_opaque,
-	},
-	background = {
-		color = colors.active.bar_bg,
-		corner_radius = 10,
-		border_color = colors.active.item_gradient[2],
-		border_width = 2,
-	},
+    position = "right",
+    padding_left = 2,
+    padding_right = 2,
+    icon = {
+        font = {
+            family = fonts.font_fira.text,
+            style = fonts.font_fira.style_map["Bold"],
+            size = fonts.font_fira.size,
+        },
+        padding_left = settings.padding.icon_label_item.icon.padding_left,
+        padding_right = 2,
+        color = colors.active.deep_blue,
+    },
+    label = {
+        font = {
+            family = fonts.font_fira.text,
+            style = fonts.font_fira.style_map["Bold"],
+            size = fonts.font_fira.size,
+        },
+        padding_left = 0,
+        padding_right = settings.padding.icon_label_item.label.padding_right,
+        color = colors.active.sep_opaque,
+    },
+    background = {
+        color = colors.active.bar_bg,
+        corner_radius = 10,
+        border_color = colors.active.item_gradient[2],
+        border_width = 2,
+    },
 })
 
 local function update_display(im_id)
@@ -61,10 +61,10 @@ input_method:subscribe("system_woke", check_status)
 check_status()
 
 input_method:subscribe("mouse.clicked", function()
-	sbar.exec("macism", function(current_id)
-		current_id = current_id:match("^%s*(.-)%s*$")
+    sbar.exec("macism", function(current_id)
+        current_id = current_id:match("^%s*(.-)%s*$")
 		local next_id = (current_id == "com.apple.keylayout.ABC") and "im.rime.inputmethod.Squirrel.Hans"
 			or "com.apple.keylayout.ABC"
-		sbar.exec("macism " .. next_id)
-	end)
+        sbar.exec("macism " .. next_id)
+    end)
 end)
