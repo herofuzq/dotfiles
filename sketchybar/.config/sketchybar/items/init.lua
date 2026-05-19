2a684a228d0e8c1e9f3886049f77e70861f43c58
herofuzq <herofuzq@gmail.com>
2026-05-19 20:59:06 +0800

    change

--- sketchybar/.config/sketchybar/items/init.lua
+++ sketchybar/.config/sketchybar/items/init.lua
@@ -5,4 +5,4 @@ require("items.spaces") -- aerospace 工作区
 require("items.calendar") -- 日期时间
 require("items.widgets") -- 右侧小部件组
 require("items.front_app") -- 当前前台应用名
-require("items.media") -- 当前播放媒体
+-- require("items.media") -- 已弃用（替换为其他软件）
