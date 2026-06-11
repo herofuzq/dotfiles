4a8dc72dbe2cbbd252a879d7305791db3d0a1aad
herofuzq <herofuzq@gmail.com>
2026-06-11 18:17:17 +0800

    fix(sketchybar/calendar): 左边padding 8→16防吞字

--- sketchybar/.config/sketchybar/items/calendar.lua
+++ sketchybar/.config/sketchybar/items/calendar.lua
@@ -59,7 +59,7 @@ local cal_items = {}
 				string = string.rep(" ", 30),
 				font = { family = "Menlo", style = "Regular", size = 10.0 },
 				color = colors.active.text,
-				padding_left = 8, padding_right = 16,
+				padding_left = 16, padding_right = 16,
 			},
 			background = { drawing = false },
 		})
