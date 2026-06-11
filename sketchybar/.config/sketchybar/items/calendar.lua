84df49267cdfd170a8c355b557396f625f90f7d9
herofuzq <herofuzq@gmail.com>
2026-06-11 18:21:33 +0800

    fix(sketchybar/calendar): 右边padding锁定40，最小不吞字值

--- sketchybar/.config/sketchybar/items/calendar.lua
+++ sketchybar/.config/sketchybar/items/calendar.lua
@@ -59,7 +59,7 @@ local cal_items = {}
 				string = string.rep(" ", 30),
 				font = { family = "Menlo", style = "Regular", size = 10.0 },
 				color = colors.active.text,
-				padding_left = 8, padding_right = 35,
+				padding_left = 8, padding_right = 40,
 			},
 			background = { drawing = false },
 		})
