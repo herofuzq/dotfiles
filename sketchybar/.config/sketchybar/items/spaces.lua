945af69c30274bc7123b654eabc6024b3bb3495b
herofuzq <herofuzq@gmail.com>
2026-06-14 22:25:12 +0800

    fix(spaces): pill右边padding=6

--- sketchybar/.config/sketchybar/items/spaces.lua
+++ sketchybar/.config/sketchybar/items/spaces.lua
@@ -635,7 +635,7 @@ for i = 1, SPACE_COUNT do
 			padding_left = 10, padding_right = 10, string = SPACE_ICONS[i] .. " >" },
 		label = { color = style.label.color, highlight_color = style.label.color, font = style.label.font,
 			padding_left = 6, padding_right = 12, y_offset = 0, drawing = false,
-			background = { drawing = true, color = 0xff585b70, corner_radius = 9, padding_left = 8, padding_right = 8 } },
+			background = { drawing = true, color = 0xff585b70, corner_radius = 9, padding_left = 8, padding_right = 6 } },
 	})
 	_n_workspaces[ws_name] = ws
 	_n_ws_order[#_n_ws_order + 1] = ws_name
