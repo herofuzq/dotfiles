658a50ef08063a17822aac602deabbfe7e9a8c21
herofuzq <herofuzq@gmail.com>
2026-06-14 22:51:38 +0800

    fix(spaces): label左右padding对称6px

--- sketchybar/.config/sketchybar/items/spaces.lua
+++ sketchybar/.config/sketchybar/items/spaces.lua
@@ -634,7 +634,7 @@ for i = 1, SPACE_COUNT do
 			font = { family = style.icon.font.family, style = style.icon.font.style, size = 15.0 },
 			padding_left = 10, padding_right = 10, string = SPACE_ICONS[i] .. " >" },
 		label = { color = style.label.color, highlight_color = style.label.color, font = style.label.font,
-			padding_left = 6, padding_right = 12, y_offset = 0, drawing = false,
+			padding_left = 6, padding_right = 6, y_offset = 0, drawing = false,
 			background = { drawing = false } },
 	})
 	_n_workspaces[ws_name] = ws
