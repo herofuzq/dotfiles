1e40ee5437e2635d3c5454abf2e415a7b23f3e43
herofuzq <herofuzq@gmail.com>
2026-06-15 11:35:03 +0800

    fix(apple): icon_width=14

--- sketchybar/.config/sketchybar/items/apple.lua
+++ sketchybar/.config/sketchybar/items/apple.lua
@@ -7,7 +7,7 @@ local colors = require("appearance").colors
 local settings = require("settings")
 
 local border_width = 0 -- 无背景无边框
-local icon_width = 16
+local icon_width = 14
 
 local function compute_icon_pad()
 	local dock_w, dock_hidden = settings.detect_dock_width()
