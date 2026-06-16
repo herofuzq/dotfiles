dd755aaa668f365bb398f0a5abb58d44e9427461
herofuzq <herofuzq@gmail.com>
2026-06-16 23:04:03 +0800

    style: 图标字体恢复13，仅widget label用12

--- sketchybar/.config/sketchybar/fonts.lua
+++ sketchybar/.config/sketchybar/fonts.lua
@@ -14,7 +14,7 @@ return {
 	font_icon = {
 		text = "Hack Nerd Font",
 		numbers = "Hack Nerd Font",
-		size = 12.0,
+		size = 13.0,
 		style_map = {
 			["Regular"] = "Regular",
 			["Semibold"] = "Semibold",
