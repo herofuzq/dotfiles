aed6102be46c94780a931821bd26ea9e83a40eeb
herofuzq <herofuzq@gmail.com>
2026-07-10 10:46:10 +0800

    fix(sketchybar): fix quit to use 'Docker Desktop' app name

--- sketchybar/.config/sketchybar/helpers/services/control.lua
+++ sketchybar/.config/sketchybar/helpers/services/control.lua
@@ -49,7 +49,7 @@ if scope == "docker" then
 		run("open -g -a Docker")
 		os.exit(0)
 	elseif action == "quit" then
-		run("osascript -e " .. shell_quote('tell application "Docker" to quit'))
+		run("osascript -e " .. shell_quote('tell application "Docker Desktop" to quit'))
 		os.exit(0)
 	end
 
